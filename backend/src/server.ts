baab90886c1234c2267c394916cc3be1b0eb17c5
Lucas Lima <lucas.alima@souunit.com.br>
2020-10-15 10:46:36 -0300

    database

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import './database/connection';
+
+const app = express();
+
+app.use(express.json())
+app.get("/", (req, res) => {
+    return res.json({message: 'Oi carai'})
+});
+
+app.listen(3333);
