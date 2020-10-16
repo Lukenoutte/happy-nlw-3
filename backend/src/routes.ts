418f8ada9c8a6a3c66b35cd748da2b028ecfbda6
Lucas Lima <lucas.alima@souunit.com.br>
2020-10-15 23:39:15 -0300

    uploads

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import OrphanagesController from './contollers/OrphanagesController';
+import multer from 'multer';
+import uploadConfig from './config/upload.ts';
 const routes = Router();
 
-
+const upload = multer(uploadConfig);
 routes.get("/orphanages", OrphanagesController.index);
 routes.get("/orphanages/:id", OrphanagesController.show);
-routes.post("/orphanages", OrphanagesController.create);
+routes.post("/orphanages", upload.array('images'),OrphanagesController.create);
 
 export default routes;
